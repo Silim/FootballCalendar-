a2cde18f5eb479b7c7bf5990518a508e4e7f5794
Silim <Silim2006@ukr.net>
2015-09-24 15:55:49 +0200

    С коллекции

---
+++ FootballCalendar/MvcApplication1/Controllers/CalendarController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class CalendarController : Controller
+    {
+        //
+        // GET: /Calendar/
+
+        public ActionResult Index()
+        {
+            var match = new List<CalendarModel>
+            {
+            new CalendarModel
+            {
+              Date = "23/09/2015 21:45",
+              Match = "Juventus - Frozinone",
+              Competition = "Seria A",
+              Chanel = "Football 1"
+            },
+            new CalendarModel
+            {
+              Date = "23/09/2015 21:45",
+              Match = "Walsall - Chelsea",
+              Competition = "English Legea Cup",
+              Chanel = "Football 2"
+            }
+            };
+            ViewBag.Matches = match;
+            return View();
+        }
+
+
+    }
+}
---
+++ FootballCalendar/MvcApplication1/Models/CalendarModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class CalendarModel
+    {
+        public string Match { get; set; }
+        public string Competition { get; set; }
+        public string Chanel { get; set; }
+        public string Date { get; set; }
+    }
+}
